c8e707e4a58fbc6e7751529dc132fdc0e24e0a28
Gabriel Felipe <gabriel.felipe@correo.ucu.edu.uy>
2023-11-04 19:30:29 -0300

    Agregando bootstrap

--- Angular/obligatorioBD/src/app/formulario/formulario.component.ts
+++ Angular/obligatorioBD/src/app/formulario/formulario.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class FormularioComponent {
 
+  ngOnInit(): void {
+    (function () {
+      'use strict'
+
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.querySelectorAll('.needs-validation')
+
+      // Loop over them and prevent submission
+      Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+          form.addEventListener('submit', function (event: any) {
+            if (!form.checkValidity()) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+          }, false)
+        })
+    })()
+  }
+
 }

4dd9c987ce7e4084954103f20ebae6e34f348756
Gabriel Felipe <gabriel.felipe@correo.ucu.edu.uy>
2023-11-13 00:05:26 -0300

    Agregando validaciones a cambiar periodo

--- Angular/obligatorioBD/src/app/cambio-fecha-periodo/cambio-fecha-periodo.component.ts
+++ Angular/obligatorioBD/src/app/cambio-fecha-periodo/cambio-fecha-periodo.component.ts
@@ -1,5 +1,42 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControlOptions } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms"
+
+
+
+
+export const dateValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  //valida que la fecha de inicio de periodo sea anterior o igual a la fecha de fin de periodo. 
+  const fchIn = control.get('fchIn');
+  const fchFin = control.get('fchVen');
+  if (!fchIn || !fchFin) {
+    //si alguno esta vacio, no verifico si tiene errores, asi lo verifica solo cuando haya
+    // ingresado la fecha de los dos
+    return null;
+  } else {
+    if (Date.parse(fchIn.value) > Date.parse(fchFin.value)) {
+      fchIn.setErrors({ 'fechasInvalidas': true });
+      fchFin.setErrors({ 'fechasInvalidas': true });
+      return { fechasInvalidas: true }
+    } else {
+      var res = false;
+      if (fchIn.hasError('fechasInvalidas')) {
+        //delete fchIn.errors['fechasInvalidas'];
+        fchIn.setErrors(null);
+        res = true;
+      }
+      if (fchFin.hasError('fechasInvalidas')) {
+        //delete fchFin.errors['fechasInvalidas'];
+        fchFin.setErrors(null);
+        res = true;
+      }
+      if (res) {
+        control.updateValueAndValidity();
+      }
+      return null;
+    }
+  }
+}
 
 @Component({
   selector: 'app-cambio-fecha-periodo',
@@ -8,9 +45,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class CambioFechaPeriodoComponent {
 
-
   date = new Date;
-  title = 'Angular Form Validation Tutorial';
   angForm: FormGroup;
   constructor(private fb: FormBuilder) {
     this.createForm();
@@ -18,8 +53,9 @@ export class CambioFechaPeriodoComponent {
 
   createForm() {
     this.angForm = this.fb.group({
-      fchVen: ['', [Validators.required]]
-    });
+      fchVen: ['', [Validators.required]],
+      fchIn: ['', [Validators.required]]
+    }, { validators: dateValidator } as AbstractControlOptions);
   }
 
 }

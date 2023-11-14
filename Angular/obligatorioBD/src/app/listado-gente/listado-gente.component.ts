6bae8c174b2ea92721d9fadfaa53b5bd30615e3f
Gabriel Felipe <gabriel.felipe@correo.ucu.edu.uy>
2023-11-14 07:55:41 -0300

    Agregando copiar a listar gente

--- Angular/obligatorioBD/src/app/listado-gente/listado-gente.component.ts
+++ Angular/obligatorioBD/src/app/listado-gente/listado-gente.component.ts
@@ -10,17 +10,35 @@ export class ListadoGenteComponent {
   angForm: FormGroup;
   personas = [{ email: 'email1@gmail.com', datos: 'Pedro Perez' },
   { email: 'email2@gmail.com', datos: 'Juan Sanchez' },
+  { email: 'email3@gmail.com', datos: 'Carlos Paz' },
+  { email: 'email3@gmail.com', datos: 'Carlos Paz' },
+  { email: 'email3@gmail.com', datos: 'Carlos Paz' },
+  { email: 'email3@gmail.com', datos: 'Carlos Paz' },
+  { email: 'email3@gmail.com', datos: 'Carlos Paz' },
+  { email: 'email3@gmail.com', datos: 'Carlos Paz' },
+  { email: 'email3@gmail.com', datos: 'Carlos Paz' },
+  { email: 'email3@gmail.com', datos: 'Carlos Paz' },
+  { email: 'email3@gmail.com', datos: 'Carlos Paz' },
+  { email: 'email3@gmail.com', datos: 'Carlos Paz' },
+  { email: 'email3@gmail.com', datos: 'Carlos Paz' },
   { email: 'email3@gmail.com', datos: 'Carlos Paz' }];
   date = '2023-11-13';
-  email = "";
   datos = "";
+  email = "";
 
   ngOnInit(): void {
 
-    for (let index = 0; index < this.personas.length; index++) {
-
-
-
+    for (let i = 0; i < this.personas.length; i++) {
+      this.datos = this.datos + this.personas[i].email + " - " + this.personas[i].datos + " \n";
+      this.email = this.email + this.personas[i].email + " \n";
     }
+
   }
+
+  copiarTexto(): void {
+
+
+
+  }
+
 }
